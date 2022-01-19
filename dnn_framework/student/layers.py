56a9bbaf0cdb5cbccc062b29aa5d98c71a5daee2
Arnaud <arnaud.judge@usherbrooke.ca>
2022-01-19 15:13:39 -0500

    implement batch norm

--- dnn_framework/student/layers.py
+++ dnn_framework/student/layers.py
@@ -49,8 +49,14 @@ class FullyConnectedLayer(Layer):
 
 class BatchNormalization(Layer):
 
-    def __init__(self):
+    def __init__(self, n):
         super().__init__()
+        self.n = n
+        self.gamma = np.ones(n)
+        self.beta = np.zeros(n)
+        self.global_mean = np.zeros(n)
+        self.global_variance = np.zeros(n)
+        self.alpha = 0.1
 
     def get_parameters(self):
         """
@@ -58,15 +64,39 @@ class BatchNormalization(Layer):
         The parameters are learned by the optimizer.
         :return: A dictionary (str: np.array) of the parameters
         """
-        raise NotImplementedError()
+        return {'gamma': self.gamma, 'beta': self.beta}
 
-    def forward(self, x):
+    def get_buffers(self):
+        """
+        This method returns the buffers of the layer.
+        The buffers are not learned by the optimizer.
+        :return: A dictionary (str: np.array) of the buffers
+        """
+        return {'global_mean': self.global_mean, 'global_variance': self.global_variance}
+
+    def forward(self, x, eps=1e-9):
         """
         This method performs the forward pass of the layer.
         :param x: The input tensor
+        :param eps: epsilon
         :return: A tuple containing the output value and the cache (y, cache)
         """
-        raise NotImplementedError()
+
+        if self.is_training():
+            u_b = x.mean(axis=0)
+            var_b = x.var(axis=0)
+
+            self.global_mean = (1 - self.alpha) * self.global_mean + self.alpha * u_b
+            self.global_variance = (1 - self.alpha) * self.global_variance + self.alpha * var_b
+
+            xhat = (x - u_b) / np.sqrt(var_b + eps)
+            cache = {'x': x, 'xhat': xhat, 'u_b': u_b, 'var_b': var_b, 'eps': eps}
+        else:
+            xhat = (x - self.global_mean) / np.sqrt(self.global_variance + eps)
+            cache = {'xhat': xhat}
+
+        y = self.gamma * xhat + self.beta
+        return y, cache
 
     def backward(self, output_grad, cache):
         """
@@ -77,7 +107,14 @@ class BatchNormalization(Layer):
                  a dictionary containing the gradient with respect to each parameter indexed with the same key
                  as the get_parameters() dictionary.
         """
-        raise NotImplementedError()
+        grad_xhat = output_grad * self.gamma
+        grad_var_b = np.sum(grad_xhat * (cache['x'] - cache['u_b']) * -1/2*((cache['var_b'] + cache['eps'])**(-3/2)), axis=0)
+        grad_u_b = (-np.sum(grad_xhat / np.sqrt(cache['var_b'] + cache['eps']), axis=0)) + (-2/cache['x'].shape[0] * grad_var_b) * np.sum((cache['x'] - cache['u_b']), axis=0)
+        grad_xi = grad_xhat / np.sqrt(cache['var_b'] + cache['eps']) + 2/cache['x'].shape[0] * grad_var_b * (cache['x'] - cache['u_b']) + 1/cache['x'].shape[0] * grad_u_b
+        grad_gamma = np.sum(output_grad * cache['xhat'], axis=0)
+        grad_beta = np.sum(output_grad, axis=0)
+
+        return grad_xi, {'gamma': grad_gamma, 'beta': grad_beta}
 
 
 class Sigmoid(Layer):
